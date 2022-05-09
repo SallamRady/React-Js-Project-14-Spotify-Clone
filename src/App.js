7bfff853112dcae6a9740d54df1c2df542e7271c
SallamRady <sallamrady@gmail.com>
2022-05-09 02:38:10 +0200

    edit structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
+import LeftSidebar from './components/leftSidebar/LeftSidebar';
+import MainContent from './components/content/MainContent';
+import RightSidebar from './components/rightSidebar/RightSidebar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LeftSidebar/>
+      <MainContent/>
+      <RightSidebar/>
+      <div className='background'></div>
     </div>
   );
 }
