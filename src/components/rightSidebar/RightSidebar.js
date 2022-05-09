a08f62c9746a62d2c6f76b9d11b176f360a8e0af
SallamRady <sallamrady@gmail.com>
2022-05-09 02:40:09 +0200

    right sidebar

---
+++ src/components/rightSidebar/RightSidebar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './RightSidebar.scss';
+
+const RightSidebar = () => {
+    return (
+        <div>
+            RightSidebar
+        </div>
+    );
+};
+
+export default RightSidebar;
